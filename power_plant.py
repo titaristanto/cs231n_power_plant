0fdaa74bfb816ab9af894d8b210231f1f3989b1e
titaristanto <titaristanto@gmail.com>
2018-05-06 16:57:35 -0700

    Update power_plant.py

--- power_plant.py
+++ power_plant.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import numpy as np
-import os, glob
+import os, glob, itertools
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 import keras
 from keras.models import Sequential
@@ -33,9 +34,57 @@ def get_data(filenames):
 
     return arr_input_X
 
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    '''
+    :param cm: a confusion matrix output from sklearn func
+    :param classes: a list of all the labels
+    :param normalize: if True, performs row normalization
+    :param title: title of the plot
+    :param cmap: color choices
+    :return:
+    '''
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix')
+
+    print(cm)
+
+    plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('Power Plant Classification Confusion Matrix.png')
+    
 def main():
     x_train = get_data(parse_filenames(folder_name='uspp_landsat'))
-
+    
+    
+    
+    
+    
+    # Show confusion Matrix
+    cnf_matrix = confusion_matrix(y_test, y_pred)
+    class_names = list([,]) # list of all the labels
+    plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix')
 
 if __name__ == '__main__':
     main()
